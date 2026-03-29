99acb69153853acfe2ad767d8682f4c1a1f37545
delveshomelappy-web <delveshomelappy@gmail.com>
2026-03-29 15:09:58 +1300

    Update src/App.jsx to include comprehensive letter image fill features.

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,31 @@
 import React, { useState } from 'react';
-import { renderLettersOnCanvas } from './utils';
+import LetterCanvas from './LetterCanvas';
 
 const App = () => {
-  const [word, setWord] = useState('');
-  const [font, setFont] = useState('Arial');
+    const [word, setWord] = useState('');
+    const [font, setFont] = useState('');
+    const [image, setImage] = useState(null);
 
-  const handleWordChange = (e) => {
-    setWord(e.target.value);
-  };
+    const handleWordChange = (e) => setWord(e.target.value);
+    const handleFontChange = (e) => setFont(e.target.value);
+    const handleImageUpload = (e) => setImage(URL.createObjectURL(e.target.files[0]));
 
-  const handleFontChange = (e) => {
-    setFont(e.target.value);
-  };
-
-  const handleRender = () => {
-    renderLettersOnCanvas(word, font);
-  };
-
-  return (
-    <div>
-      <h1>Letter Image Fill</h1>
-      <input 
-        type="text" 
-        value={word} 
-        onChange={handleWordChange} 
-        placeholder="Enter your word here" 
-      />
-      <select value={font} onChange={handleFontChange}>
-        <option value="Arial">Arial</option>
-        <option value="Verdana">Verdana</option>
-        <option value="Times New Roman">Times New Roman</option>
-        <option value="Courier New">Courier New</option>
-      </select>
-      <button onClick={handleRender}>Render</button>
-      <canvas id="canvas" width="800" height="400"></canvas>
-    </div>
-  );
+    return (
+        <div className="app">
+            <h1>Letter Image Fill</h1>
+            <input type="text" value={word} onChange={handleWordChange} placeholder="Enter your word" />
+            <select value={font} onChange={handleFontChange}>
+                <option value="">Select Font</option>
+                <option value="Arial">Arial</option>
+                <option value="Verdana">Verdana</option>
+                <option value="Times New Roman">Times New Roman</option>
+                {/* Add more fonts as needed */}
+            </select>
+            <input type="file" onChange={handleImageUpload} />
+            <LetterCanvas word={word} font={font} image={image} />
+            {/* Include tools for drag/resize functionality and export capabilities here */}
+        </div>
+    );
 };
 
 export default App;
